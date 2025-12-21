d001b1d186df4dcd7069b7dc82b721dc088688ce
nerdigenius <Ashiq.racer@gmail.com>
2025-12-21 16:35:28 +0600

    fix: update image source handling in TestimonialCard component
    - Refactor image source logic to handle both string and object formats for sideVertor, quoteImage, and profilePic.
    - Ensure compatibility with various image import methods.

--- src/components/ui/TestimonialCard.tsx
+++ src/components/ui/TestimonialCard.tsx
@@ -11,9 +11,9 @@ type TestimonialCardProps = {
 const TestimonialCard = ({ quote, authorName, location }: TestimonialCardProps) => {
   return (
     <div className="flex flex-col justify-between w-full lg:w-[45%] bg-yellow-500 border-2 px-[9%] lg:px-[5%] py-[5%] relative text-gray-800 h-[335px] lg:h-[555px] ">
-      <img src={sideVertor.src || sideVertor} alt="" className="lg:block absolute left-0 bottom-0 lg:bottom-10 w-8" />
+      <img src={typeof sideVertor === 'string' ? sideVertor : sideVertor.src} alt="" className="lg:block absolute left-0 bottom-0 lg:bottom-10 w-8" />
       <div className="w-full">
-        <img src={quoteImage.src || quoteImage} alt="" className="h-4 w-4" />
+        <img src={typeof quoteImage === 'string' ? quoteImage : quoteImage.src} alt="" className="h-4 w-4" />
         <p className="text-lg font-roboto text-left pl-4">{quote} </p>
       </div>
 
@@ -23,7 +23,7 @@ const TestimonialCard = ({ quote, authorName, location }: TestimonialCardProps) 
           <p className="text-xs font-roboto">{location}</p>
         </div>
         <div className="h-full w-fit  border-b-4 border-b-red-800 z-20 absolute right-0 ">
-          <img src={profilePic.src || profilePic} alt="Author" className="w-10 h-10 rounded-full " />
+          <img src={typeof profilePic === 'string' ? profilePic : profilePic.src} alt="Author" className="w-10 h-10 rounded-full " />
         </div>
 
       </div>
